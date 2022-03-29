4815794a07b6e092e2cdadc0e3852d9c4c05db15
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-03-29 09:41:39 +0200

    konec hodiny

--- OOP_Chess_IT1A/Figure.cs
+++ OOP_Chess_IT1A/Figure.cs
@@ -6,11 +6,38 @@ namespace OOP_Chess_IT1A
 {
     public class Figure
     {
+        private List<string> numbers = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        private string position;
+
         public FigureType Type { get; set; }
+        public string Position
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))    //hodnota v proměnné value je null nebo ""
+                {
+
+                }
+                else if (value.Length == 2)
+                {
+                    if (numbers.Contains(value.Substring(1, 1)))  //druhý znak v proměnné value je v listu numbers
+                    {
+
+                    }
+                }
+                else
+                {
+
+                }
+            }
+            get => position;
+        }
 
         public Figure()
         {
-            
+            Position = "A2";
+            Position = "a2";
+            Position = "sdfsdf";
         }
     }
 
