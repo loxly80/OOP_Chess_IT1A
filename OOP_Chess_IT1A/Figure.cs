2f8138bd2439cfb52f3b84f09a5f94ca448d28ed
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-03-29 09:12:57 +0200

    Add project files.

---
+++ OOP_Chess_IT1A/Figure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Chess_IT1A
+{
+    public class Figure
+    {
+        public FigureType Type { get; set; }
+
+        public Figure()
+        {
+            
+        }
+    }
+
+
+    //výčtový datový typ - seznam hodnot - interně jsou to hodnoty integer (od 0)
+    public enum FigureType
+    {
+        Pawn,       //pěšec
+        Rook,       //věž
+        Knight,     //jezdec
+        Bishop,     //střelec
+        Queen,      //dáma
+        King        //král
+    }
+}
